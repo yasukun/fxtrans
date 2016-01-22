a240d47d6cf99f59ddc29758540c8259837d9a27
miyazaki <miyazaki_masayasu_f5@lab.ntt.co.jp>
2016-01-22 16:01:37 +0900

    add burst

--- app/Main.hs
+++ app/Main.hs
@@ -4,13 +4,28 @@ import System.Environment
 import Data.Text (pack)
 import Lib
 import Control.Monad
+import Data.List (nub)
+import Algo.Lib
+
+-- main :: IO ()
+-- main = do
+--   args <- getArgs
+--   case args of
+--     [] -> print "done"
+--     _  -> forM_ args (\x -> do
+--                         csv <- readFile x
+--                         let (Right result) = quoteParser2 $ pack csv
+--                         print $ map (\x -> mergeQuote x) $ groupQuote result)
+
 
 main :: IO ()
 main = do
   args <- getArgs
   case args of
     [] -> print "done"
-    _  -> forM_ args (\x -> do
-                        csv <- readFile x
-                        let (Right result) = quoteParser2 $ pack csv
-                        print $ map (\x -> mergeQuote x) $ groupQuote result)
+    _ -> forM_ args (\x -> do
+                       csv <- readFile x
+                       let (Right result) = quoteParser2 $ pack csv
+                           -- result2 = map (\x -> mergeQuote x) $ groupQuote result
+                       print $ kleinberg (tstoargs result) defOpts
+                    )
--- src/Lib.hs
+++ src/Lib.hs
@@ -6,22 +6,23 @@ module Lib
     , quoteParser2
     , groupQuote
     , mergeQuote
+    , tstoargs
     ) where
 
+import Control.Monad
 import Control.Applicative
-import qualified Data.Vector as V
-import qualified Data.Vector.Unboxed as U
 import Data.Attoparsec.Text
 import Data.Attoparsec.Combinator
-import Data.Time
 import Data.List
 import Data.Text (Text, unpack, pack)
 import qualified Data.Text as T
 import Data.Maybe
 import Text.Printf
+import qualified Data.ByteString.Char8 as B
+import Data.UnixTime
 
 data Quote = Quote {
-      qTime :: LocalTime,
+      qTime :: UnixTime,
       qOpen :: Double,
       qHigh :: Double,
       qLow :: Double,
@@ -42,11 +43,11 @@ csvFile = do
 qcomma  :: Parser ()
 qcomma = char ',' *> pure ()
 
-qtime   :: Parser LocalTime
+qtime :: Parser UnixTime
 qtime = createTime <$> takeTill (\x -> x == ',')
-        where defaultTime = LocalTime (fromGregorian 0001 01 01) (TimeOfDay 00 00 00)
-              parseTimeText t = parseTimeM False defaultTimeLocale "%Y/%m/%d %H:%M" (unpack $ roundTime 5 t)
-              createTime x = fromMaybe defaultTime $ parseTimeText x
+        where defaultTime = (UnixTime 0 0)
+              parseTimeText t = parseUnixTime (B.pack "%Y/%m/%d %H:%M" :: Format) (B.pack $ unpack t)
+              createTime x = fromMaybe defaultTime $ Just $ parseTimeText x
 
 quote :: Parser Quote
 quote = Quote <$> (qtime <* qcomma)
@@ -67,7 +68,6 @@ quoteParser = parseOnly quote
 quoteParser2 = parseOnly csvFile
 
 roundTick :: Integral a => a -> a -> a
-roundTick tick 60 = 0
 roundTick tick sec= tick + ((sec `div` tick) - 1) * tick
 
 roundTime :: Int -> Text -> Text
@@ -94,3 +94,10 @@ mergeQuote qs = foldl (\x y -> let (Quote {  qVolume = qv
                 Quote {qTime = lTime, qOpen = lOpen, qClose = lClose, qVolume = 0, qHigh = 0, qLow = 1/0} qs
         where (Quote {qTime = lTime, qOpen = lOpen}) = head qs
               (Quote {qClose = lClose}) = last qs
+
+tstoargs (x:xs) = map (\y -> let (Quote {qTime = UnixTime {utSeconds = targetTS}}) = y
+                          in (read (show targetTS) :: Int) - startINT) xs'
+    where Quote {qTime = UnixTime {utSeconds = startTS}} = x
+          startINT = read (show startTS) :: Int
+          xs' = filter (\y -> let (Quote {qOpen = targetOP, qClose = targetCS}) = y
+                              in targetOP > targetCS) (x:xs)
